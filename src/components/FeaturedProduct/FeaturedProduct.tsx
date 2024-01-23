12aedd7533a07eebf25034f838f8ab7746329a63
Sean Borland <seanbor123@gmail.com>
2024-01-23 16:08:30 -0500

    final edit

--- src/components/FeaturedProduct/FeaturedProduct.tsx
+++ src/components/FeaturedProduct/FeaturedProduct.tsx
@@ -11,10 +11,10 @@ const FeaturedProduct = () => {
           className="featured-product-image"
         />
         <div className="product-details">
-          <h2>Ruby Voodoo Rose - A Mesmerizing Floral Marvel</h2>
+          <h2>Ruby Voodoo Rose</h2>
           <p className="product-price">$29.99</p>
           <p className="product-description">
-          The Ruby Voodoo Rose stands as a symbol of enchanting beauty and mystery in the world of floriculture. With its unique deep ruby red petals interlaced with sultry purple hues, this rose variety presents a captivating sight that's both alluring and mystifying.
+          The Ruby Voodoo Rose stands as a symbol of enchanting beauty and mystery in the world of floriculture.
           </p>
         </div>
       </div>
